5191cb9a80f5ee840bb605e6902a3ded5392d588
Ayrlin <39847734+Ayrlin-Renata@users.noreply.github.com>
2024-01-01 23:30:17 -0600

    prep for chapter load implementation

---
+++ blade-heart/src/components/LabelMenuItem.tsx
@@ -0,0 +1,17 @@
+
+interface LabelMenuItem {
+    id: string
+    content: string
+    subContent?: string
+}
+
+export default function ({ id, content, subContent }: LabelMenuItem) {
+    return (
+        <>
+            <div class="menuitem labelmenuitem" id={id}>
+                <div class="content"> {content} </div>
+                <div class="subcontent"> {subContent} </div>
+            </div>
+        </>
+    )
+}
--- blade-heart/src/components/ReaderMenu.tsx
+++ blade-heart/src/components/ReaderMenu.tsx
@@ -1,15 +1,29 @@
 import { useState } from 'preact/hooks';
 
+import { content as listContent } from '../assets/json/contentlist.json';
+import { content as metaContent } from '../assets/json/contentmeta.json';
+
 import ReaderMenuHeader from './ReaderMenuHeader';
+import LabelMenuItem from './LabelMenuItem';
 import AccountMenuItem from './AccountMenuItem';
 import SelectMenuItem from './SelectMenuItem';
 import MenuDivider from './MenuDivider';
 
-export default function () {
+export default function ( { location } : { location: any} ) {
+    const urlPage: string = (location.pathname.split('/').length > 0
+    ? location.pathname.split('/')[location.pathname.split('/').length - 1]
+    : 'default-word');
+    const mangaTitle = decodeURIComponent(urlPage);
+    const mangaInfo = listContent.find((obj) => obj.title === mangaTitle);
+    const mangaMeta: any = metaContent[urlPage as keyof typeof metaContent];
+    if(!mangaInfo || !mangaMeta) {
+        console.warn("manga info not found!");
+        return (<div class="readermenu">{"manga info not found!"}</div>);
+    }
+    
     const [ collapsed, setCollapsed ] = useState("false");
 
     const collapsedBool: boolean = collapsed === "true";
-
 
     return (
         <>
@@ -19,7 +33,15 @@ export default function () {
                     <ReaderMenuHeader />
                     <AccountMenuItem />
                     <MenuDivider />
-                    <SelectMenuItem id="mangalanguage" label="language" options={["EN","CN","JP"]} />
+                    <LabelMenuItem id="mangatitle" 
+                        content={ mangaTitle } 
+                        subContent={ mangaInfo.subtitle } />
+                    <SelectMenuItem id={ mangaTitle + "/mangalanguage" }  
+                        label="Language" 
+                        options={mangaInfo.languages} />
+                    <SelectMenuItem id={ mangaTitle + "/mangachapter" }  
+                        label="Chapter" 
+                        options={mangaMeta.chapters.map((obj:any) => obj.authornumber + ": " + obj.name)} />
                 </div>
 
             </div>
